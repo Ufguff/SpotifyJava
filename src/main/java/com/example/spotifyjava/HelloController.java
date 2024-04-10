9fb5ee5b31cdc11e8b90bc134bb40df83ed1dc01
Ufguff <tima.shchipkov@mail.ru>
2024-04-10 21:19:51 +0500

    Разбирательсво с fxml

--- src/main/java/com/example/spotifyjava/HelloController.java
+++ src/main/java/com/example/spotifyjava/HelloController.java
@@ -1,16 +1,19 @@
 package com.example.spotifyjava;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    protected Label LabelSH;
+    private Label Label;
 
     @FXML
-    private void clickButton()
-    {
+    private Button ButtonCl;
 
+    public void clickButton()
+    {
+        Label.setVisible(true);
     }
 
 }
